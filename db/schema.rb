c68b4fdd241fc71d7211214f8dc78a41e7cc9891
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.modem>
2022-02-18 21:05:11 +1100

    added email field to users because thats what jwt wants

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_054217) do
+ActiveRecord::Schema.define(version: 2022_02_18_085945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questionbanks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ print "Creating Users..."
 User.destroy_all
 
 
-u1 = User.create! name: 'alex', password: 'chicken'
-u2 = User.create! name: 'angela', password: 'chicken'
-u3 = User.create! name: 'cassie', password: 'chicken'
-u4 = User.create! name: 'ethan', password: 'chicken'
+u1 = User.create! name: 'alex', password: 'chicken', email: 'alex@ga.co'
+u2 = User.create! name: 'angela', password: 'chicken', email: 'angela@ga.co'
+u3 = User.create! name: 'cassie', password: 'chicken', email: 'cassie@ga.co'
+u4 = User.create! name: 'ethan', password: 'chicken', email: 'ethan@ga.co'
 
 puts "Created #{User.count} users."
