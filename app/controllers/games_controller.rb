44c4dbb60730512542c7f10de787f1acec200ae0
Angela Zhang <a_zhang93@hotmail.com>
2022-02-23 09:50:12 +1100

    pulling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,29 +1,29 @@
 class GamesController < ApplicationController
 
-    def create 
-        game = Game.new(game_params)
-        conversation = Conversation.find(game_params[:room_id])
-        if game.save
-          serialized_data = ActiveModelSerializers::Adapter::Json.new(
-            GameSerializer.new(game)
-          ).serializable_hash
-          GamesChannel.broadcast_to room, serialized_data
-          head :ok
-        end
-
+  def create 
+    game = Game.new(game_params)
+    conversation = Conversation.find(game_params[:room_id])
+    if game.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        GameSerializer.new(game)
+      ).serializable_hash
+      GamesChannel.broadcast_to room, serialized_data
+      head :ok
     end
 
-    def show
-        game = Game.find params[:id]
-        GamesChannel.broadcast_to 
-    end
+  end
+
+  def show
+    game = Game.find params[:id]
+    GamesChannel.broadcast_to 
+  end
 
 
-    private
-      
-      def game_params
-        params.require(:game).permit(:title, :room_id)
-      end
+  private
+    
+  def game_params
+    params.require(:game).permit(:title, :room_id)
+  end
 
 
 
