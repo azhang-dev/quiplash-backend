3fc095508b30a53281748136761b631cf138ee8d
Angela Zhang <a_zhang93@hotmail.com>
2022-02-23 17:28:48 +1100

    commented out user_token controller

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,11 +1,11 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
 
-  def create
-    cookies.signed["session"] = {user_id: entity.id}
-    # cookies.signed["user_id"] = entity.id
-    p "<><><><><><><><><><>", cookies.signed["session"]
-    super  #  all this does is:  "render json: auth_token, status: :created"
-  end
+  # def create
+  #   cookies.signed["session"] = {user_id: entity.id}
+  #   # cookies.signed["user_id"] = entity.id
+  #   p "<><><><><><><><><><>", cookies.signed["session"]
+  #   super  #  all this does is:  "render json: auth_token, status: :created"
+  # end
 
 end
