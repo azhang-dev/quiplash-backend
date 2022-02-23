1137601f1e87dd47ff829333a2fbf202e26bae90
Angela Zhang <a_zhang93@hotmail.com>
2022-02-24 09:39:38 +1100

    fixed add_colum question bank to questionbank

--- db/migrate/20220223104052_add_questionbank_id_to_questionbank.rb
+++ db/migrate/20220223104052_add_questionbank_id_to_questionbank.rb
@@ -1,5 +1,5 @@
 class AddQuestionbankIdToQuestionbank < ActiveRecord::Migration[5.2]
   def change
-    remove_column :questionbanks, :questionbank_id, :integer
+    add_column :questionbanks, :questionbank_id, :integer
   end
 end
