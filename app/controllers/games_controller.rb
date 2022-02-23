54940816e6598c3dd97377f04d4cad04905b14f1
Angela Zhang <a_zhang93@hotmail.com>
2022-02-23 16:59:02 +1100

    fixed syntax in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,17 @@
 class GamesController < ApplicationController
 
-    def create 
-        game = Game.new(game_params)
-        room = Room.find(game_params[:room_id])
-        if game.save
-          serialized_data = ActiveModelSerializers::Adapter::Json.new(
-            GameSerializer.new(game)
-          ).serializable_hash
-          GamesChannel.broadcast_to room, serialized_data
-          head :ok
-        end
-
+  def create 
+    game = Game.new(game_params)
+    room = Room.find(game_params[:room_id])
+    if game.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        GameSerializer.new(game)
+      ).serializable_hash
+      GamesChannel.broadcast_to room, serialized_data
+      head :ok
     end
+
+  
 
   end
 
