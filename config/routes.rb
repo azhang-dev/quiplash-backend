9e9de507fcca896bb6e47d456ed9533c22c4ae90
Angela Zhang <a_zhang93@hotmail.com>
2022-02-24 11:54:45 +1100

    merged

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
   get '/users/current' => 'users#current'
   post '/user' => 'user#create'
-  post '/room/edit/:id' => 'rooms#update'
+  put '/room/edit/:id' => 'rooms#update'
   put '/room/start/:id' => 'rooms#start'
   resources :rooms, only: [:create, :index, :destroy, :show, :update]
   resources :questions, only: [:show]
   resources :answers, only: [:create, :show]
   resources :votes, only: [:create, :index]
   resources :games, only: [:create, :show, :index]
-  post '/questionbanks' => "questionbank#create"
-
+  post '/questionbanks' => "questionbanks#create"
+  #
 
   
   
