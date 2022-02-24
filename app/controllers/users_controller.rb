6de6e201b18eb54ce68db5b7bd593dd9d711624c
Alex King <calicoalix@gmail.com>
2022-02-24 18:22:24 +1100

    pulling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, except:[:create]
 
   def current
     render json: current_user
   end
 
   def create
+    headers['Access-Control-Allow-Origin'] = '*'
+
     user = User.create! user_params
 
     if user.persisted?
