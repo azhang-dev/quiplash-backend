7a88b3a9158fb5dc0f2e49d7e94c4dd804eea85e
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.modem>
2022-02-25 02:21:01 +1100

    added image urls to questions(locations) database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_140330) do
+ActiveRecord::Schema.define(version: 2022_02_24_145810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_140330) do
   end
 
   create_table "questionbanks", force: :cascade do |t|
-    t.boolean "default"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
@@ -50,10 +49,11 @@ ActiveRecord::Schema.define(version: 2022_02_24_140330) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "question"
     t.integer "questionbank_id"
+    t.string "image"
   end
 
   create_table "rooms", force: :cascade do |t|
