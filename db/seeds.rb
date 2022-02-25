f76446d9007c51090978ea66dc13773e93475867
Angela Zhang <a_zhang93@hotmail.com>
2022-02-25 15:14:02 +1100

    uncommented questionbank in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -434,19 +434,19 @@ u1.questionbanks << b1
 # puts "the answer of #{Question.first.question} is ..."
 
 
-# print "creating QuestionBank..."
-# Questionbank.destroy_all
-# b1 = Questionbank.create!(
-#     name: "Random questions",
-# )
+print "creating QuestionBank..."
+Questionbank.destroy_all
+b1 = Questionbank.create!(
+    name: "Random questions",
+)
 
-# b2 = Questionbank.create!(
-#     name: "GA related questions",
-# )
+b2 = Questionbank.create!(
+    name: "GA related questions",
+)
 
-# puts "created #{Questionbank.count} questionbanks."
-# b1.questions << q1<< q2 << q3 << q4 << q6 << q7 << q8 << q9 << q16 << q17 << q18 << q19 << q20
-# b2.questions << q5 << q10 << q11 << q12 << q13 << q14 << q15 << q21
+puts "created #{Questionbank.count} questionbanks."
+b1.questions << q1<< q2 << q3 << q4 << q6 << q7 << q8 << q9 << q16 << q17 << q18 << q19 << q20
+b2.questions << q5 << q10 << q11 << q12 << q13 << q14 << q15 << q21
 
 
 
